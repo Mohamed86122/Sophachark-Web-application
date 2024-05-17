2fbf9f16ccc9a47348fa51e6fb6bf5be103f7b35
Omar <Oelmahi31@gmail.com>
2024-05-17 21:41:16 +0100

    Ajout message de refus

--- SophaTemp/Areas/Admin/Controllers/LoginController.cs
+++ SophaTemp/Areas/Admin/Controllers/LoginController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using SophaTemp.Data;
 using SophaTemp.Mappers;
 using SophaTemp.Viewmodel;
@@ -12,22 +13,38 @@ namespace SophaTemp.Controllers
     {
         private readonly AppDbContext _context;
         private readonly PersonMapper _personMapper;
+        private readonly ILogger<LoginController> _logger;
 
-        public LoginController(AppDbContext context, PersonMapper mapper)
+
+        public LoginController(AppDbContext context, PersonMapper mapper, ILogger<LoginController> logger)
         {
             _context = context;
             _personMapper = mapper;
+            _logger = logger;
+
         }
 
         [HttpGet]
         public IActionResult Login()
         {
+            // Récupérer le message d'erreur de la session
+            var errorMessage = HttpContext.Session.GetString("ErrorMessage");
+            if (!string.IsNullOrEmpty(errorMessage))
+            {
+                ModelState.AddModelError("", errorMessage);
+                // Effacer le message d'erreur après l'avoir affiché
+                HttpContext.Session.Remove("ErrorMessage");
+            }
+
+            _logger.LogInformation("Displaying login page.");
             return View();
         }
 
         [HttpPost]
         public IActionResult Login(PersonVm model)
         {
+            _logger.LogInformation("Attempting to log in.");
+
             if (ModelState.IsValid)
             {
                 var user = _context.Personnes
@@ -36,7 +53,7 @@ namespace SophaTemp.Controllers
 
                 if (user != null)
                 {
-                    // Usage Session
+                    // Stocker les informations de l'utilisateur dans la session
                     HttpContext.Session.SetString("UserId", user.PersonneId.ToString());
                     HttpContext.Session.SetString("UserEmail", user.email);
                     HttpContext.Session.SetString("UserRole", user.Passeport?.Nom ?? "User");
