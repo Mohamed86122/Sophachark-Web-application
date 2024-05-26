2e97b80c844694117baf8c082936e391b1e40dfc
Omar <Oelmahi31@gmail.com>
2024-05-26 23:36:20 +0100

    Edit Facture , Edite et delete Passport

--- SophaTemp/Models/Facture.cs
+++ SophaTemp/Models/Facture.cs
@@ -1,25 +1,25 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿    using System.ComponentModel.DataAnnotations.Schema;
 
-namespace SophaTemp.Models
-{
-    public class Facture
+    namespace SophaTemp.Models
     {
-        public int FactureId { get; set; }
-        public string Numero { get; set; }
-        public double Montant { get; set; }
-        public DateTime DateFacturation { get; set; }
+        public class Facture
+        {
+            public int FactureId { get; set; }
+            public string Numero { get; set; }
+            public double Montant { get; set; }
+            public DateTime DateFacturation { get; set; }
 
-        public int CommandeId { get; set; }
+            public int CommandeId { get; set; }
 
-        [ForeignKey("CommandeId")]
-        public Commande Commande { get; set; }
+            [ForeignKey("CommandeId")]
+            public Commande Commande { get; set; }
 
         
 
-        public void GenerationFacture()
-        {
+            public void GenerationFacture()
+            {
+
+            }
 
         }
-
     }
-}
--- SophaTemp/Viewmodel/FactureVm.cs
+++ SophaTemp/Viewmodel/FactureVm.cs
@@ -2,12 +2,9 @@
 {
     public class FactureVm
     {
-       
-            public int FactureId { get; set; }
-            public string Numero { get; set; }
-            public double Montant { get; set; }
-            public DateTime DateFacturation { get; set; }
-
-            public int CommandeId { get; set; }
+        public string Numero { get; set; }
+        public double Montant { get; set; }
+        public DateTime DateFacturation { get; set; }
+        public int CommandeId { get; set; }
     }
 }
